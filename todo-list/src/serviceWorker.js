16b77ff6af80c4a60e0456b1e46feb77965afdfa
Vladimir Zeynalov <v.zeinalov@kt-team.de>
2019-05-08 10:56:05 +0400

    register sw

--- todo-list/src/serviceWorker.js
+++ todo-list/src/serviceWorker.js
@@ -21,7 +21,7 @@ const isLocalhost = Boolean(
 );
 
 export function register(config) {
-  if (process.env.NODE_ENV === 'production' && 'serviceWorker' in navigator) {
+  if ('serviceWorker' in navigator) {
     // The URL constructor is available in all browsers that support SW.
     const publicUrl = new URL(process.env.PUBLIC_URL, window.location.href);
     if (publicUrl.origin !== window.location.origin) {
@@ -43,7 +43,7 @@ export function register(config) {
         navigator.serviceWorker.ready.then(() => {
           console.log(
             'This web app is being served cache-first by a service ' +
-              'worker. To learn more, visit https://bit.ly/CRA-PWA'
+              'worker.'
           );
         });
       } else {
