cffdf665995d5f95efbc066304b5c05f5e40bdb3
Vladimir <99percent6@gmail.com>
2019-06-14 07:52:06 +0400

    added build dist

---
+++ todo-list/build/precache-manifest.107619c04f146f4b804e4b4fe7ef06c2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "9ba625cc3a4ea44187d0",
+    "url": "/static/js/main.4f35ee3f.chunk.js"
+  },
+  {
+    "revision": "6b68f42fa3c343f429a6",
+    "url": "/static/js/2.0163391b.chunk.js"
+  },
+  {
+    "revision": "9ba625cc3a4ea44187d0",
+    "url": "/static/css/main.4281f935.chunk.css"
+  },
+  {
+    "revision": "a39b811b5016d8744712cc6f03e7901a",
+    "url": "/index.html"
+  }
+];
