9f03344bdd8c1a04d49108b2ce3bcc4ba743ba41
Владимир Зейналов <99percent6@gmail.com>
2020-02-02 12:43:23 +0400

    update dist

--- todo-list/dist/service-worker.js
+++ todo-list/dist/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "precache-manifest.9924b6bbe34efe9a051c0deaeb484bc8.js"
+  "precache-manifest.8921081c6c9c31fa564806147bcd035c.js"
 );
 
 self.addEventListener('message', (event) => {
